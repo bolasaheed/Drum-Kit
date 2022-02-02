633844f645113461768a137f6c09ee4278671078
Bola Saheed <bola_saheed@yahoo.com>
2022-02-03 00:50:34 +0100

    completet js script file

---
+++ script/script.js
@@ -0,0 +1,23 @@
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+}
+
+function playSound(e) {
+  const audio = document.querySelector('audio[data-key="' + e.keyCode + '"]');
+  const key = document.querySelector('.key[data-key="' + e.keyCode + '"]');
+  if (!audio) return;
+
+  key.classList.add('playing');
+  audio.currentTime = 0;
+  audio.play();
+}
+
+function addKeyEventListeners(key) {
+  key.addEventListener('transitionend', removeTransition)
+}
+
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(addKeyEventListeners);
+window.addEventListener('keydown', playSound);
